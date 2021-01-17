dba9dc0540a4d9c68b8b22042c1d5705a9ab9460
Vivekanand Kini <onlinekini@gmail.com>
2021-01-17 18:00:47 +0530

    union find incomplete

---
+++ src/unionfind/UnionFind.java
@@ -0,0 +1,16 @@
+package com.coursera.algo.part1.unionfind;
+
+public class UnionFind {
+    //UnionFind(int count); constructor to add the integers in the array of union find
+
+    void union(int p, int q); // union between two points p & q
+
+    boolean connected(int p, int q); //Check if p & q are connected.
+
+
+    public static  void main(String args []) {
+        // read the input from file. unionFindTest.txt
+        // first entry is the count
+        // next entries are for unions
+    }
+}
